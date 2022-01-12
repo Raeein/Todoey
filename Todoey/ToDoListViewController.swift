f8157ed9d9144450c7bac1e3ef43214241207c9b
Raeein Bagheri <raeeinbagheri80@gmail.com>
2022-01-12 14:05:52 -0500

    Implement the TableView

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = [
+        "Find Mike",
+        "Hello",
+        "Bye"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = itemArray[indexPath.row]
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    
+}
+
