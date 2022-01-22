05e8481b38446b3314e115b8ccf587402b82f379
Raeein Bagheri <raeein@aol.com>
2022-01-22 16:46:05 -0500

    Implement all CRUD functionality and UISearchBar

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -9,6 +9,7 @@ class ToDoListViewController: UITableViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         loadItems()
     }
     
@@ -31,10 +32,17 @@ class ToDoListViewController: UITableViewController, UITextFieldDelegate {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        //        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        //          to update
+        //        itemArray[indexPath.row].setValue("Completed", forKey: "title")
+        
+        //to delete
+        //        context.delete(itemArray[indexPath.row])
+        //        itemArray.remove(at: indexPath.row)
+        
+//        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         itemArray[indexPath.row].done.toggle()
-        tableView.deselectRow(at: indexPath, animated: true)
         saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
         
     }
     
@@ -99,14 +107,24 @@ class ToDoListViewController: UITableViewController, UITextFieldDelegate {
         }
         self.tableView.reloadData()
     }
-    func loadItems() {
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
         do {
             
             itemArray = try context.fetch(request)
         } catch {
             print("Error fetching data from context\(error)")
         }
+    }
+    
+}
+//MARK: - Search bar methods
+extension ToDoListViewController: UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+
+        loadItems(with: request)
     }
 }
 
